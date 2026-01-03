da43a5b01728b0f6939e2623ebf9478b60c13c88
Srijan0113 <141817720+Srijan0113@users.noreply.github.com>
2026-01-03 10:45:53 +0530

    Delete College-Project directory

--- College-Project/test1.py
+++
@@ -1,6 +0,0 @@
-import pygame
-pygame.mixer.init()
-pygame.mixer.music.load(r"C:\Users\Acer\Desktop\college-project\focus_system\alert1.wav")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy():
-    pass
